6187a145396583f6708729293c2ef7d6e9315dc9
Paula Figueiredo <paulafigueiredo36@gmail.com>
2025-12-09 05:15:24 -0300

    Feat: sobe atualizacoes

--- cat-design-system-angular/src/app/app.routes.ts
+++ cat-design-system-angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CatButtonComponent } from './componentes/buttons/cat-button/cat-button';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [ 
+    { path: 'localhost:4200', component: CatButtonComponent } // Rota principal
+];
